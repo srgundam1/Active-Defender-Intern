e35c1410e2ca58ca14d96516938b3a050460dc1e
srgundam1 <106888384+srgundam1@users.noreply.github.com>
2022-07-16 15:01:10 -0400

    Add files via upload

---
+++ Popup.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class Popup : MonoBehaviour
+{
+    [SerializeField] Button popupButton;
+    [SerializeField] Text message;
+
+    public void Init(Transform canvas, string txt) 
+    {
+        message.text = txt;
+
+        transform.SetParent(canvas);
+        Vector3 scale = new Vector3(3.7f, 3.7f, 1);
+        transform.localScale = scale;
+        transform.localPosition = Vector3.zero;
+        popupButton.onClick.AddListener(() =>
+        {
+            GameObject.Destroy(this.gameObject);
+        });
+
+    }
+}
+
